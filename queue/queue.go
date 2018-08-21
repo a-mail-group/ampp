5ec3b9ad7267568115ffe236b9e565773dbb04f4
Simon Schmidt <simon.schmidt@htp-tel.de>
2018-08-21 13:47:54 +0200

    queue 3

--- queue/queue.go
+++ queue/queue.go
@@ -19,8 +19,6 @@ import "time"
 
 import "github.com/a-mail-group/ampp/qmodel"
 
-type Message qmodel.Message
-
 type Queue struct{
 	DB *bolt.DB
 }
@@ -37,12 +35,12 @@ func (q *Queue) Process(f func(*Tx) error) error {
 type Tx struct{
 	tx *bolt.Tx
 }
-func (tx *Tx) EnqueueMessage(queue string,msg *Message) error {
+func (tx *Tx) EnqueueMessage(queue string,msg *qmodel.Message) error {
 	key := make([]byte,0,len(time.RFC3339Nano))
 	key = time.Now().UTC().AppendFormat(key,time.RFC3339Nano)
 	return tx.ReEnqueueMessage(key,queue,msg)
 }
-func (tx *Tx) ReEnqueueMessage(key []byte,queue string,msg *Message) error {
+func (tx *Tx) ReEnqueueMessage(key []byte,queue string,msg *qmodel.Message) error {
 	data,err := msgpack.Marshal(msg)
 	if err!=nil { return err }
 	bkt,err := tx.tx.CreateBucketIfNotExists([]byte(queue))
@@ -51,12 +49,12 @@ func (tx *Tx) ReEnqueueMessage(key []byte,queue string,msg *Message) error {
 	return bkt.Put(key,data)
 }
 
-func (tx *Tx) DequeueMessage(queue string) (key []byte,msg *Message,err error) {
+func (tx *Tx) DequeueMessage(queue string) (key []byte,msg *qmodel.Message,err error) {
 	bkt := tx.tx.Bucket([]byte(queue))
 	if bkt==nil { err = io.EOF; return }
 	k,v := bkt.Cursor().First()
 	if len(k)==0 { err = io.EOF; return }
-	msg = new(Message)
+	msg = new(qmodel.Message)
 	err = msgpack.Unmarshal(v,msg)
 	if err!=nil { msg = nil; return }
 	key = make([]byte,len(k))
@@ -67,6 +65,6 @@ func (tx *Tx) Enqueue(queue string,from string, to []string, r io.Reader) error 
 	data,err := ioutil.ReadAll(r)
 	if err!=nil { return err }
 	if len(data) > 3<<9 { return smtp.ErrDataTooLarge }
-	return tx.EnqueueMessage(queue,&Message{from,to,data})
+	return tx.EnqueueMessage(queue,&qmodel.Message{from,to,data})
 }
 
